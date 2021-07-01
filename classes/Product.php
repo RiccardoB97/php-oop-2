bc9393fcf8de65a2cca47ea3359f4f0c3d255e08
RiccardoB97 <70477170+RiccardoB97@users.noreply.github.com>
2021-07-01 16:46:05 +0200

    products now printed on screen instead of var_dump

--- classes/Product.php
+++ classes/Product.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Product{
     public $name;
     public $price;
--- index.php
+++ index.php
@@ -5,5 +5,27 @@ require __DIR__ . '/classes/PremiumUser.php';
 require __DIR__ . '/classes/CreditCard.php';
 require __DIR__ . '/database/db.php';
 
+?>
 
-var_dump($products);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP-2</title>
+</head>
+<body>
+    <main>
+        <?php foreach ($products as $product): ?>
+            <div>
+                <h2><?= strtoupper($product->name)?></h2>
+                <span>Item price: <?= $product->price?> - </span>
+                <span>In stock: <?= $product->qnty?></span>
+            </div>
+        <? endforeach; ?>
+    </main>
+
+
+</body>
+</html>
